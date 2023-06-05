d768e6f75d9f04279b75c20304b83e7f9390e9a6
yamadahideto <yahideto428@gmail.com>
2023-06-05 23:47:06 +0900

    deletefunction_complete

--- public/js/product.js
+++ public/js/product.js
@@ -1,57 +1,29 @@
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   $(function () {
-//     $(".deleteBtn").click(function (event) {
-//       // event.preventDefault(); // 削除処理無効化
-//       var id = $(this).data('product_id');
-//       $.ajax({
-//         headers: { //HTTPヘッダ情報をヘッダ名と値のマップで記述
-//           'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//         },
-//         type: "POST",
-//         // url: 'destroy/' + id,
-//         url: "http://localhost:8888/Vending-management/public/destroy/" + id,
-//         dataType: "json",
-//         // data: {"id": id },
-//       // }).done(function (data) {
-//         success: function (response) {
-//           // 削除が成功した場合の処理
-//           alert(id);
-//           console.log(response);
-//         },
-//         // }).fail(function (data) {
-//         error: function (xhr, status, error) {
-//           // エラー時の処理
-//           alert(id + "失敗しました");
-//           console.error("error" +error);
-//           console.log("response", xhr.responseText);
-//         }
-//       })
-//     });
-
-//   });
-// });
-
-
 document.addEventListener("DOMContentLoaded", function () {
   $(function () {
     $(".deleteBtn").click(function (event) {
       var id = $(this).data('product_id');
-      $.ajax({
-        headers: { //HTTPヘッダ情報をヘッダ名と値のマップで記述
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        type: "POST",
-        url: "http://localhost:8888/Vending-management/public/destroy/" + id,
-        dataType: "json",
-        success: function (response) { // 削除が成功した場合の処理
-          alert(id + "の削除が成功しました!");
-        },
-        error: function (response,xhr, status, error) { // エラー時の処理
-          console.error("error" + error);
-          console.log("response", xhr.responseText);
-        }
-      })
+      var deleteButton = $(this);
+      var deleteModal = confirm("削除してよろしいですか？"); //削除して良いかの確認モーダル
+      if (deleteModal) {
+        $.ajax({
+          headers: { //HTTPヘッダ情報をヘッダ名と値のマップで記述
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          },
+          type: "POST",
+          url: "http://localhost:8888/Vending-management/public/destroy/" + id,
+          dataType: "json",
+          success: function (response) { // 削除が成功した場合の処理
+            deleteButton.closest("tr").remove();
+          },
+          error: function (response, xhr, status, error) { // エラー時の処理
+            console.error("error" + error);
+            console.log("response", xhr.responseText);
+          }
+        })
+      }else{
+        // キャンセル処理
+        // 特に何もしない
+      }
     });
 
   });
